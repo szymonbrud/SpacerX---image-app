71394ce7211c9199a00fc74d67f63d90e00e3bdf
szymonqqaz <szymonqqaz22@gmail.com>
2019-05-18 18:54:00 +0200

    Edit aboutpage.js and add new icon to app

--- src/Components/AboutPage/AboutPage.js
+++ src/Components/AboutPage/AboutPage.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import bg from '../../img/bg.png';
-import Logo from '../Logo/Logo';
-import AboutButton from '../AboutButton/AboutButton';
-import media from '../../utlis/media';
+import React, { Component } from "react";
+import styled from "styled-components";
+import bg from "../../img/bg.png";
+import Logo from "../Logo/Logo";
+import AboutButton from "../AboutButton/AboutButton";
+import media from "../../utlis/media";
 
-import { connect } from 'react-redux';
-import { AboutPageVisible } from '../../actions/ApiRequest';
+import { connect } from "react-redux";
+import { AboutPageVisible } from "../../actions/ApiRequest";
 
 const WrapperAbout = styled.div`
   width: 100%;
@@ -15,21 +15,24 @@ const WrapperAbout = styled.div`
   left: 0;
   top: 0;
   margin: 0;
-  padding: 0; 
+  padding: 0;
   background: url(${bg});
   background-size: 100% 100%;
   z-index: 9000;
 
   animation-name: spadanie;
-  animation-fill-mode: forwards;  
-  animation-duration: .4s;
+  animation-fill-mode: forwards;
+  animation-duration: 0.4s;
   animation-iteration-count: infinite;
   animation-iteration-count: 1;
 
-  @keyframes spadanie
-  {
-    0% { opacity: 0; }
-    100% { opacity: 1; }
+  @keyframes spadanie {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
   }
 `;
 
@@ -39,10 +42,10 @@ const H1 = styled.h1`
   font-size: 1.4rem;
   margin: 15vh 0 0 6%;
 
-  @media(orientation: landscape){
+  @media (orientation: landscape) {
     margin: 20vh 0 0 6%;
   }
-  @media(max-width: 900px) and (orientation: landscape){
+  @media (max-width: 900px) and (orientation: landscape) {
     font-size: 1.1rem;
   }
 
@@ -58,8 +61,8 @@ const Text1 = styled.p`
   display: flex;
   align-items: center;
 
-  @media(max-width: 900px) and (orientation: landscape){
-    font-size: .8rem;
+  @media (max-width: 900px) and (orientation: landscape) {
+    font-size: 0.8rem;
   }
 
   ${media.fullHd`
@@ -69,13 +72,13 @@ const Text1 = styled.p`
 
 const Text2 = styled.p`
   padding: 0;
-  margin: 10vh 6%;
+  margin: ${({ two }) => (two ? "1vh" : "6vh")} 6% 2vh;
   font-size: 1.1rem;
   display: flex;
   align-items: center;
 
-  @media(max-width: 900px) and (orientation: landscape){
-    font-size: .8rem;
+  @media (max-width: 900px) and (orientation: landscape) {
+    font-size: 0.8rem;
   }
 
   ${media.fullHd`
@@ -83,88 +86,64 @@ const Text2 = styled.p`
   `}
 `;
 
-const KontaktDiv = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 15vh;
-  background: rgba(213,75,61,.8);
-  border-top: solid black .5px;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-
-  @media(orientation: landscape){
-    flex-direction: inherit;
-    align-items: center;
-  }
-
-  ${media.desktop`
-    left: 50%;
-    width: 50%
-    height: 10vh;
-    border: none;
-  `}
-`;
-
-const P = styled.p`
-  color: white;
-  font-weight: 500;
-  font-size: 1.2rem;
-  margin: .5vh 6%;
-
-  @media(max-width: 900px) and (orientation: landscape){
-    font-size: .8rem;
-  }
-
-  ${media.desktop`
-    font-size: .6rem;
-  `}
-  ${media.fullHd`
-    font-size: 1.2rem;
-  `}
-`;
-
-
-
-class AboutPage extends Component{
-  
-  render(){
-
+class AboutPage extends Component {
+  render() {
     const { AboutPageVisible } = this.props;
 
-    return(
+    return (
       <>
         <WrapperAbout>
           <Logo>SpacerX</Logo>
-          <AboutButton onClick={() => AboutPageVisible(false)}>Back</AboutButton>
+          <AboutButton onClick={() => AboutPageVisible(false)}>
+            Back
+          </AboutButton>
           <H1>Hello,</H1>
           <Text1>
-            Aplikacja jest przeznaczona do oglądania zdjęć kosmosu oraz innych obiektów związanych ze zdjęciami satelitarnymi oraz bezpośrednio z NASA. Wpisujecie co chcielibyście zobaczyć (oczywiście po angielsku), a waszym oczom ukazują się zdjęcia o danym zapytaniu. Aplikacja została stworzona ponieważ chciałem doskonalić się jako Frontend Developer
-            w react oraz redux właśnie w tych technologiach została napisana owa aplikacja.
+            Aplikacja jest przeznaczona do oglądania zdjęć kosmosu oraz innych
+            obiektów związanych ze zdjęciami satelitarnymi oraz bezpośrednio z
+            NASA. Wpisujecie co chcielibyście zobaczyć (oczywiście po
+            angielsku), a waszym oczom ukazują się zdjęcia o danym zapytaniu.
+            Aplikacja została stworzona ponieważ chciałem doskonalić się jako
+            Frontend Developer w react oraz redux właśnie w tych technologiach
+            została napisana owa aplikacja.
           </Text1>
           <Text2>
-            Jeśli ktoś byłby ciekawy kodu lub chciał się ze mną skontaktować to daję link do mojego GitHuba.
+            <div>
+              Ikonka logo aplikacji jest z:
+              <br />
+              Icons made by{" "}
+              <a href="https://www.freepik.com/" title="Freepik">
+                Freepik
+              </a>{" "}
+              from{" "}
+              <a href="https://www.flaticon.com/" title="Flaticon">
+                www.flaticon.com
+              </a>{" "}
+              is licensed by{" "}
+              <a
+                href="http://creativecommons.org/licenses/by/3.0/"
+                title="Creative Commons BY 3.0"
+                target="blank"
+              >
+                CC 3.0 BY
+              </a>
+            </div>
           </Text2>
-          <KontaktDiv>
-            <P>Link do mojego GitHuba:</P>
-            <P>Wkrótce dostępny</P>
-          </KontaktDiv>
         </WrapperAbout>
-      </> 
-    )
+      </>
+    );
   }
 }
 
 const mapStateToProps = () => {
-  return{
-    
-  }
-}
+  return {};
+};
 
 const mapActionToProps = {
-  AboutPageVisible: AboutPageVisible,
-}
+  AboutPageVisible: AboutPageVisible
+};
 
-export default connect(mapStateToProps, mapActionToProps) (AboutPage);
+export default connect(
+  mapStateToProps,
+  mapActionToProps
+)(AboutPage);
--- src/index.js
+++ src/index.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-import thunk from 'redux-thunk';
-import { applyMiddleware, compose, createStore, combineReducers } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import thunk from "redux-thunk";
+import { applyMiddleware, compose, createStore, combineReducers } from "redux";
 // import { composeWithDevTools } from 'redux-devtools-extension';
-import { Provider } from 'react-redux';
-import { apiReducer, loadingReducer, StateOfImages, AboutPageReducer } from './Reducers/ApiReducer';
+import { Provider } from "react-redux";
+import {
+  apiReducer,
+  loadingReducer,
+  StateOfImages,
+  AboutPageReducer
+} from "./Reducers/ApiReducer";
 
 const allReducers = combineReducers({
   ApiImages: apiReducer,
   loading: loadingReducer,
   StateOfMyApp: StateOfImages,
-  AboutPageVisible: AboutPageReducer,
-})
+  AboutPageVisible: AboutPageReducer
+});
 
 const allStoreEnchancers = compose(
-  applyMiddleware(thunk),
+  applyMiddleware(thunk)
   // composeWithDevTools(),
-)
+);
 
-const store = createStore(
-  allReducers,
-  allStoreEnchancers,
-)
+const store = createStore(allReducers, allStoreEnchancers);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
-  </Provider> ,document.getElementById('root')
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
-
-serviceWorker.unregister();
